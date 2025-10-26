f0f2a4fef1dff65e25d74b6ce87504c24342d4ee
Yash Yadav <yadavyash8185@gmail.com>
2025-10-26 23:41:45 +0530

    Update EvenOdd.java
    Using ternary operator

--- EvenOdd.java
+++ EvenOdd.java
@@ -8,3 +8,14 @@ public class EvenOdd {
             System.out.println(num + " is Odd.");
     }
 }
+// Another way without if else by using ternary operator
+
+import java.util.Scanner;
+public class Evenodd{
+    public static void main(String[] args){
+        System.out.print("Enter the nuumber : ");
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        System.out.print("The Number is : " + ((s % 2==0) ? "Even" : "Odd"));
+    }
+}
