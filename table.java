e6901ff415a27531ef5a9f1308559b7dc5d27d06
Yash Yadav <yadavyash8185@gmail.com>
2025-11-11 23:11:33 +0530

    Create table.java

---
+++ table.java
@@ -0,0 +1,27 @@
+/*
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
+2 x 5 = 10
+2 x 6 = 12
+2 x 7 = 14
+2 x 8 = 16
+2 x 9 = 18
+2 x 10 = 20
+*/
+//Problem statement
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i=1;i<=10;i++){
+            int result=n*i;
+            System.out.println(n +" x " +i +" = " +result );
+        }
+    }
+}
